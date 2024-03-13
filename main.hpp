055d342cf569fd6c0c7fd1063fcc130ade58da22
spsalazar <156946827+spsalazar@users.noreply.github.com>
2024-03-13 19:00:12 +0000

    finished prodjuct

--- main.hpp
+++ main.hpp
@@ -19,7 +19,41 @@ int getRandom(void)
 }
 int getClosestMed(int rdnum1, int rdnum2, int rdnum3)
 {
-       double mean = (rdnum1 + rdnum2 + rdnum3) / 3;
+        double mean = (rdnum1 + rdnum2 + rdnum3) / 3;
+        int closest;
+        int newrdnum1, newrdnum2, newrdnum3;
 
-        if (rdnum1 > )
+        if (mean > rdnum1) {
+                newrdnum1 = mean - rdnum1;
+        }
+        else if (mean > rdnum2) {
+                newrdnum2 = mean - rdnum2;
+        }
+        else if (mean > rdnum3) {
+                newrdnum3 = mean - rdnum3;
+        }
+
+        if (rdnum1 > mean) {
+                newrdnum1 = rdnum1 - mean;
+        }
+        else if (rdnum2 > mean) {
+                newrdnum2 = rdnum2 - mean;
+        }
+        else if (rdnum3 > mean) {
+                newrdnum3 = rdnum3 - mean;
+        }
+
+        if (newrdnum1 > newrdnum2 && newrdnum1 > newrdnum3) {
+                closest = rdnum1;
+        }
+        else if (newrdnum2 > newrdnum1 && newrdnum2 > newrdnum3) {
+                closest = rdnum2;
+        }
+        else if (newrdnum3 > newrdnum1 && newrdnum3 > newrdnum2) {
+                closest = rdnum3;
+        }
+        return closest;
 }
+
+//64 75 86
+//10 1 12
